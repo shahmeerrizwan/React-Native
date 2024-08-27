8d1f286b022cec78f1c48dd577b08470afd803e5
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-08-27 14:32:45 +0500

    Components

---
+++ Secound Project/app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import Button from "../Components/Button";
+
+
+export default function HomeScreen() {
+  return (
+    <View>
+  <Text style={{fontSize:30, textAlign:"center",top:80,fontWeight:700}}> Components</Text>
+  <Button  />
+  
+  </View>
+  );
+}
+
---
+++ Secound Project/app/Components/Button.tsx
@@ -0,0 +1,10 @@
+
+import { View , Button} from 'react-native'
+
+export default function ButtonComponent() {
+  return (
+    <View>   
+   <Button  title="Login"></Button>
+    </View>
+  )
+}
