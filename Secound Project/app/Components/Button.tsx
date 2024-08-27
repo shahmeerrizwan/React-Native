65498e17ce74bb44f0b76c131e161b08477d76a7
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-08-28 00:29:01 +0500

    Components/Functions

--- Secound Project/app/Components/Button.tsx
+++ Secound Project/app/Components/Button.tsx
@@ -1,10 +1,13 @@
-
-import { View , Button} from 'react-native'
+import { View, Button } from "react-native";
 
 export default function ButtonComponent() {
+  const alert = () => {
+    console.warn("Function Call Sucessfully");
+  };
+
   return (
-    <View>   
-   <Button  title="Login"></Button>
+    <View>
+      <Button onPress={alert} color={'red'} title="Login"></Button>
     </View>
-  )
+  );
 }
