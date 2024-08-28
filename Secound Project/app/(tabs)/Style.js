cc3c00663bbf5f27e31151cc48cf4a6df974f216
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-08-29 00:09:23 +0500

    Components/Functions/Props/Styling

---
+++ Secound Project/app/(tabs)/Style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export default ExStyle = StyleSheet.create({
+    Externalstyle: {
+        top: 90,
+        textAlign: "center",
+        fontWeight: "700",
+        fontSize: 30,
+        backgroundColor: 'orange',
+        padding: 10,
+    }
+})
--- Secound Project/app/(tabs)/index.tsx
+++ Secound Project/app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
-import { Text, View } from "react-native";
+import { Text, View ,StyleSheet} from "react-native";
 import Button from "../Components/Button";
 import { useState } from "react";
+import ExStyle from './Style'
 
 
 export default function HomeScreen() {
@@ -10,8 +11,23 @@ const[name,setName]=useState("Signup")
   return (
     <View>
   <Text style={{fontSize:30, textAlign:"center",top:80,fontWeight:700}}> Components</Text>
+
   <Button title={name} onPress={()=>{setName("Login")}} />
+   
+    <Text style={{fontSize:30, textAlign:"center",top:80,fontWeight:700}}>Inline Style</Text>
+    <Text style={Style.addStyle} >Internal Style</Text>
+    <Text style={ExStyle.Externalstyle}>External Style</Text>
   </View>
   );
 }
 
+const Style=StyleSheet.create({
+  addStyle:{
+    top:90,
+    textAlign:"center",
+    fontWeight:"700",
+    fontSize:30,
+    backgroundColor:'yellow',
+    padding:10
+  }
+})
