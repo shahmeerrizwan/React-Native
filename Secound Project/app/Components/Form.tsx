0bfe43c41230746e8a624be14efb9cbb3186f0c4
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-08-31 00:09:35 +0500

    Form Validation

--- Secound Project/app/Components/Form.tsx
+++ Secound Project/app/Components/Form.tsx
@@ -1,24 +1,69 @@
-import { useState } from "react"
-import { Text, TextInput, TouchableOpacity, View } from "react-native"
+import { useState } from "react";
+import {
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function Form() {
-const [userName,setUserName]=useState("")
-const [email,setEmail]=useState("")
-const [password,setPassword]=useState("")
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [display, setDisplay] = useState(false);
 
+  const clearDetail = () => {
+    setDisplay(false);
+    setEmail("");
+    setPassword("");
+    setUserName("");
+  };
 
   return (
     <>
       <View>
-<Text>User Form</Text>
-<TextInput placeholder="Enter Your Name" onChangeText={(text)=>{setUserName(text)}}/>
-<TextInput placeholder="Enter Your Email" onChangeText={(text)=>{setEmail(text)}}/>
-<TextInput placeholder="Enter Your Password" onChangeText={(text)=>{setPassword(text)}}/>
+        <Text>User Form</Text>
+        <TextInput
+          placeholder="Enter Your Name"
+          onChangeText={(text) => {
+            setUserName(text);
+          }}
+        />
+        <TextInput
+          placeholder="Enter Your Email"
+          onChangeText={(text) => {
+            setEmail(text);
+          }}
+        />
+        <TextInput
+          placeholder="Enter Your Password"
+          onChangeText={(text) => {
+            setPassword(text);
+          }}
+        />
 
-<TouchableOpacity>
-    <Text></Text>
-</TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            setDisplay(true);
+          }}
+        >
+          <Text>Print Details</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Clear Details</Text>
+        </TouchableOpacity>
+
+        <View>
+          <Text>Detailed</Text>
+          {display ? (
+            <View>
+              <Text>User Name is :{userName}</Text>
+              <Text>User Email is :{email}</Text>
+              <Text>User Password is :{password}</Text>
+            </View>
+          ) : null}
         </View>
+      </View>
     </>
-  )
+  );
 }
